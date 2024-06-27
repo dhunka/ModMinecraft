63f4335e174053a21ffbcb00a19e01532aa3cbc0
dhunka <matiasguzman45@gmail.com>
2024-06-27 02:00:24 -0400

    commit inicial

---
+++ src/main/java/miprimermodcom/exampled/MiPrimerMod.java
@@ -0,0 +1,16 @@
+package miprimermodcom.exampled;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MiPrimerMod implements ModInitializer {
+    public static final String MOD_ID = "miprimermod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/miprimermodcom/exampled/MiPrimerModClient.java
@@ -0,0 +1,10 @@
+package miprimermodcom.exampled;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MiPrimerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
